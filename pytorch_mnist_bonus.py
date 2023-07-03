0898fed6cbfbc1337e6896059d22131f7efe9063
오민섬 <alstja981101@naver.com>
2023-07-04 02:07:08 +0900

    latest

--- pytorch_mnist_bonus.py
+++ pytorch_mnist_bonus.py
@@ -29,10 +29,8 @@ class My_Model(nn.Module):
         out_data = self.layer(in_data)       
         return out_data
 
-
 bs = 4           
-learning_rate = 0.0005
-num_epochs = 20			# Epoch 수 고정
+num_epochs = 20
 data_dir = "./"		
 
 mnist_train = dset.MNIST(root=data_dir, train=True,  transform=transforms.ToTensor(), download=True)
@@ -45,12 +43,7 @@ mnist_test  = torch.utils.data.Subset(mnist_test , range(0, 1000))
 train_loader = DataLoader(mnist_train, batch_size=bs, shuffle=True, drop_last=True)
 test_loader  = DataLoader(mnist_test,  batch_size=bs, shuffle=True, drop_last=True)
 
-
-model = My_Model()
-loss_func = nn.CrossEntropyLoss()		 	
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)   
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")		# cuda는 GPU를 의미함.		
-model = model.to(device)					
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")		
 
 def train(train_loader):
     for (data, target) in train_loader:
@@ -75,27 +68,37 @@ def evaluate(test_loader):
     test_accuracy = 100. * correct / num_test_data
     return test_accuracy
 
-train_acc_list = [10]			
-test_acc_list = [10]			
 
+learning_rates = [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01]  
+train_accuracies = {lr: [] for lr in learning_rates}
+test_accuracies = {lr: [] for lr in learning_rates}
 
-for epoch in range(1, num_epochs + 1):
-    train(train_loader)			# 학습 실시
-    train_accuracy = evaluate(train_loader)	# 학습 정확도 계산
-    test_accuracy = evaluate(test_loader)	# 테스트 정확도 계산
+for learning_rate in learning_rates:
+    model = My_Model()
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    loss_func = nn.CrossEntropyLoss()  
+    train_acc_list = [10]
+    test_acc_list = [10]
 
-    train_acc_list.append(train_accuracy.item())	# 학습 데이터에 대한 정확도 리스트 갱신
-    test_acc_list.append(test_accuracy.item())	# 시험 데이터에 대한 정확도 리스트 갱신
+    for epoch in range(1, num_epochs + 1):
+        train(train_loader)
+        train_accuracy = evaluate(train_loader) 
+        test_accuracy = evaluate(test_loader)
+        train_acc_list.append(train_accuracy.item())
+        test_acc_list.append(test_accuracy.item())
+        print(f'Learning rate: {learning_rate}  Epoch:{epoch:2d}   Train Acc: {train_accuracy:6.2f}%   Test Acc: {test_accuracy:5.2f}%')
 
-    print(f'Epoch:{epoch:2d}   Train Acc: {train_accuracy:6.2f}%   Test Acc: {test_accuracy:5.2f}%')
+    train_accuracies[learning_rate] = train_acc_list
+    test_accuracies[learning_rate] = test_acc_list
 
-x = np.arange(len(train_acc_list))
+for lr in learning_rates:
+    x = np.arange(len(train_accuracies[lr]))
+    plt.plot(x, train_accuracies[lr], label=f'train acc, lr={lr}')
+    plt.plot(x, test_accuracies[lr], label=f'test acc, lr={lr}', linestyle='--')
 
-plt.plot(x, train_acc_list, label='train acc')	
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')		
-
-plt.xlabel("epoch_num")			
-plt.ylabel("accuracy (%)")			
-plt.ylim(0, 100.0)			
-plt.legend(loc='lower right')		
+plt.xlabel("epoch_num")
+plt.ylabel("accuracy (%)")
+plt.ylim(0, 100.0)
+plt.legend(loc='lower right')
 plt.show()
